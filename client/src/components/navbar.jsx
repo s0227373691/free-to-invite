bdd96a2d254169756296de134182e8cb592f17f0
Elmer <s0227373691@gmail.com>
2020-10-14 16:58:06 +0800

    new buttons

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -2,14 +2,39 @@ import React from 'react';
 import styled from 'styled-components';
 
 const NavBar = () => {
-    return <Navbar></Navbar>;
+    return (
+        <Navbar>
+            <Btn>登入</Btn>
+            <Btn>註冊</Btn>
+        </Navbar>
+    );
 };
 
 export default NavBar;
 
 const Navbar = styled.nav`
     width: 100%;
-    background: red;
-    height: 100px;
+    height: 80px;
     margin: 0 auto;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+`;
+
+const Btn = styled.button`
+    height: min-content;
+    margin: 5px;
+    padding: 15px;
+    border-radius: 50%;
+    font-size: 18px;
+    background: white;
+    outline-style: none;
+    border: 0px;
+    cursor: pointer;
+
+    &:hover {
+        color: white;
+        background: gray;
+        transition: 0.3s;
+    }
 `;

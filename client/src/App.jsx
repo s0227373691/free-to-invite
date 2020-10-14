a2dad61efc55ad65314457a5cd4afada05dd01ee
Elmer <s0227373691@gmail.com>
2020-10-14 15:35:55 +0800

    new Menu component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import Menu from './components/menu';
 import Navbar from './components/navbar';
 import Main from './components/main';
 import Login from './components/login';
@@ -8,8 +9,9 @@ import Login from './components/login';
 const App = () => {
     return (
         <div>
-            <Navbar />
+            <Menu />
             <Router>
+                <Navbar />
                 <Main />
                 <Route path="/login" component={Login} />
             </Router>

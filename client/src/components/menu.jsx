21fbee994dc90ccc1d825d83d04da7b4b2356479
js0731 <a0905050792@gmail.com>
2020-10-14 15:50:56 +0800

    new menu

--- client/src/components/menu.jsx
+++ client/src/components/menu.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Menu = () => {
-    return <div>Menu</div>;
+    return <Aside></Aside>;
 };
 
+const Aside = styled.aside`
+    width: 300px;
+    height: 100vh;
+    background: #74b9ff;
+`;
 export default Menu;

f81d531c6aa9804b93b18817c9049b3ff67996cc
atain050 <207410290@gms.tku.edu.tw>
2020-10-16 16:21:20 +0800

    set member component

--- client/src/components/member.jsx
+++ client/src/components/member.jsx
@@ -4,8 +4,14 @@ import styled from 'styled-components';
 const Member = () => {
     return (
         <div>
-            Member
-            <ColorDiv />
+            <ColorDiv3>
+                Public profile <hr />
+            </ColorDiv3>
+            <ColorDiv>
+                Name
+                <input type="text" />
+            </ColorDiv>
+
             <ColorDiv1 />
             <ColorDiv2 />
         </div>
@@ -15,17 +21,22 @@ const Member = () => {
 export default Member;
 
 const ColorDiv = styled.div`
-    width: 100px;
-    height: 100px;
+    width: auto;
+    height: 40px;
     background-color: red;
 `;
 const ColorDiv1 = styled.div`
     width: 100px;
-    height: 100px;
+    height: 40px;
     background-color: yellow;
 `;
 const ColorDiv2 = styled.div`
     width: 100px;
-    height: 100px;
+    height: 40px;
     background-color: blue;
+`;
+
+const ColorDiv3 = styled.div`
+    /* font-size: 50px; */
+    font-size: 20px;
 `;

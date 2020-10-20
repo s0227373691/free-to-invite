3f09457991400a2ff1072b324881e8aa19cde0f5
js0731 <a0905050792@gmail.com>
2020-10-20 14:10:05 +0800

    edit App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Menu from './components/menu';
 import Navbar from './components/navbar';
-import Main from './components/main';
+// import Main from './components/main';
 import Login from './components/login';
+import Member from './components/member';
 
 const App = () => {
     return (
         <Router>
-            <Menu />
-            <Switch>
-                <Navbar />
-                <Main />
-                <Route path="/login" component={Login} />
-            </Switch>
+            <Container>
+                <Menu />
+                <Main>
+                    <Switch>
+                        <Navbar />
+                        <Route path="/login" component={Login} />
+                        <Route path="/member" component={Member} />
+                    </Switch>
+                </Main>
+            </Container>
         </Router>
     );
 };
 
 export default App;
+
+const Container = styled.div`
+    display: flex;
+    width: 100%;
+`;
+const Main = styled.div`
+    flex: auto;
+`;

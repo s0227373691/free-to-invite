2586e8e6896dc397a148a7269d4af2574e7528d6
Elmer <s0227373691@gmail.com>
2020-10-22 23:35:48 +0800

    套用button tag 以及 輸入表單更新數值

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import setInputValue from '../lib/setInputValue';
+
+import Button from './commom/baseTag/button';
 
 const Header = () => {
+    const [search, setSearch] = useState('');
+
     return (
         <Head>
-            <Btn>登入</Btn>
-            <Btn>註冊</Btn>
+            <MenuBtn>三</MenuBtn>
+            <Search
+                value={search}
+                onChange={(e) => setInputValue(e, setSearch)}
+            />
+            <BtnLogin>登入</BtnLogin>
+            <BtnLogin>註冊</BtnLogin>
         </Head>
     );
 };
@@ -14,30 +24,34 @@ export default Header;
 
 const Head = styled.head`
     width: 100%;
-    height: 64px;
     padding: 8px;
-    margin: 0 auto;
     display: flex;
     justify-content: flex-end;
     align-items: center;
+    box-sizing: border-box;
     box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px,
         rgba(0, 0, 0, 0.23) 0px 6px 6px;
 `;
 
-const Btn = styled.button`
+const MenuBtn = styled(Button)`
+    padding: 5px;
+`;
+
+const Search = styled.input`
+    width: 500px;
+    height: 35px;
+`;
+
+const BtnLogin = styled(Button)`
     height: min-content;
     margin: 5px;
     padding: 15px;
-    border-radius: 50%;
+    border-radius: 5px;
     font-size: 18px;
-    background: white;
-    outline-style: none;
-    border: 0px;
-    cursor: pointer;
 
     &:hover {
-        color: white;
-        background: gray;
+        color: #ffffff;
+        background: #c4c4c4;
         transition: 0.3s;
     }
 `;

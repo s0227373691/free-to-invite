8f5c9f540073543c5e304e1bde89eebf080bf0ea
js0731 <a0905050792@gmail.com>
2020-10-26 12:20:02 +0800

    edit newArticle component

--- client/src/components/newArticle.jsx
+++ client/src/components/newArticle.jsx
@@ -4,20 +4,24 @@ import styled from 'styled-components';
 const NewArticle = () => {
     return (
         <Container>
-            <select required>
-                <option value="" hidden>
-                    請選擇分類
-                </option>
-                <option value="1">Option 1</option>
-                <option value="2">Option 2</option>
-                <option value="3">Option 3</option>
-                <option value="4">Option 4</option>
-                <option value="5">Option 5</option>
-            </select>
-            <input id="date" type="date"></input>
-            <input type="text" placeholder="人數" />
-            <input type="text" placeholder="標題" />
-            <textarea name="" id="" cols="50" rows="20"></textarea>
+            <Form action="">
+                <Upperlock>
+                    <Select required>
+                        <option value="" hidden>
+                            請選擇分類
+                        </option>
+                        <option value="1">休閒</option>
+                        <option value="2">運動</option>
+                        <option value="3">旅遊</option>
+                    </Select>
+                    <input id="date" type="date"></input>
+                    <input type="text" placeholder="人數" />
+                </Upperlock>
+                <LowerBlock>
+                    <input type="text" placeholder="標題" />
+                    <textarea name="" id="" cols="50" rows="20"></textarea>
+                </LowerBlock>
+            </Form>
         </Container>
     );
 };
@@ -25,3 +29,10 @@ const NewArticle = () => {
 export default NewArticle;
 
 const Container = styled.div``;
+const Form = styled.form``;
+const Upperlock = styled.div``;
+const LowerBlock = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+const Select = styled.select``;

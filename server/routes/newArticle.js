ac27ecf83424f52794038cce898c600e67935fbe
Elmer <s0227373691@gmail.com>
2020-11-03 17:01:28 +0800

    新增 newArticle 資料功能

---
+++ server/routes/newArticle.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const _ = require('lodash');
+
+const { NewArticle } = require('../modules/newArticle');
+
+router.get('/', (req, res) => {
+    res.send('newArticle');
+});
+router.post('/', async (req, res) => {
+    const data = req.body;
+    const newArticle = new NewArticle(
+        _.pick(req.body, ['board', 'date', 'people', 'title', 'content'])
+    );
+    await newArticle.save();
+    res.send(data);
+});
+
+module.exports = router;

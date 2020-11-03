9ee09d6dee07cc4115736895369131db4ded8023
js0731 <a0905050792@gmail.com>
2020-11-03 17:01:20 +0800

    add json file

---
+++ main.js
@@ -0,0 +1,32 @@
+const menu = [
+    {
+        primaryType: '休閒',
+        content: [
+            {
+                minorType: '桌游',
+                url: '/abc',
+                value: 'type',
+            },
+            {
+                minorType: '籃球',
+                url: '/abc',
+                value: 'type',
+            },
+        ],
+    },
+    {
+        primaryType: '休閒',
+        content: [
+            {
+                minorType: '桌游',
+                url: '/abc',
+                value: 'type',
+            },
+            {
+                minorType: '籃球',
+                url: '/abc',
+                value: 'type',
+            },
+        ],
+    },
+];

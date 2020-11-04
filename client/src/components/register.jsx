c8c7688a36d4b09620709a92b68e4e1e0d366a3d
Elmer <s0227373691@gmail.com>
2020-11-04 19:07:54 +0800

    route register獨立一份js檔

--- client/src/components/register.jsx
+++ client/src/components/register.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import Axios from 'axios';
 
 import BackDrop from './commom/backDrop';
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,29 +1,12 @@
-const _ = require('lodash');
-const bcrypt = require('bcrypt');
-const { User, validate } = require('../modules/user');
-const jwt = require('jsonwebtoken');
-const Joi = require('joi');
-
 const express = require('express');
 const router = express.Router();
 
 const auth = require('./users/auth');
+const register = require('./users/register');
 
-router.post('/register', async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-
-    let user = await User.findOne({ email: req.body.email });
-    if (user) return res.status(400).send('User already registered');
-
-    user = new User(_.pick(req.body, ['name', 'email', 'password']));
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(user.password, salt);
-    await user.save();
-
-    res.send(_.pick(user, ['_id', 'name', 'email']));
-});
 
 router.use('/auth', auth);
+router.use('/register', register);
+
 
 module.exports = router;

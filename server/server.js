bd31ad51cd7056cd1deec14ea4ee6602be3dd6ac
Elmer <s0227373691@gms.tku.edu.tw>
2020-11-05 00:36:36 +0800

    移除舊route auth版本

--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ const app = express();
 const verifyJWT = require('./middleware/verifyJWT');
 
 const users = require('./routes/users');
-const auth = require('./routes/auth');
 const newArticle = require('./routes/newArticle');
 const connectDB = require('./config/db');
 
@@ -46,7 +45,6 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/users', users);
-// app.use('/api/auth', auth);
 app.use('/api/newarticle', newArticle);
 
 app.get('/api/test', verifyJWT, (req, res) => {

5ed0eac61452ba836301f3e940eda1ad6b3faac3
js0731 <a0905050792@gmail.com>
2020-12-18 14:19:53 +0800

    update file

--- client/src/components/googleMap.jsx
+++ client/src/components/googleMap.jsx
@@ -1,26 +1,15 @@
 import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 
-import { SelectClearDefault } from '../styles/selects';
-import { ButtonClearDefault } from '../styles/buttons';
-import { TextareaClearDefault } from '../styles/textarea';
-
-import { postNewActiveBoardGame } from '../../lib/api/newActive/boardGame';
 import { InputClearDefault } from '../styles/inputs';
 
-import dateFormat from 'dateformat';
-
-import IconPrice from '../../assets/svg/price';
-import IconCalendar from '../../assets/svg/calendar';
-import IconTitle from '../../assets/svg/title';
 import IconPlace from '../../assets/svg/place';
-import IconPopulation from '../../assets/svg/population';
 
 // -------------------------------------------------------------------------------------------------
 //標題 人數 時間 地點 補充說明
 import GoogleMapReact from 'google-map-react';
 
-const Boardgame = (props) => {
+const GoogleMap = (props) => {
     // 當地圖載入完成，將地圖實體與地圖 API 傳入 state 供之後使用
     const [mapInstance, setMapInstance] = useState(null);
     const [mapApi, setMapApi] = useState(null);
@@ -112,153 +101,9 @@ const Boardgame = (props) => {
             }
         );
     };
-    // ---------------------------------------------------------------------------------------
-
-    const now = dateFormat(new Date(), 'isoDate');
-
-    const [population, setPopulation] = useState('');
-    const [boardGameType, setBoardGameType] = useState('');
-    const [cost, setCost] = useState('');
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
-    const [backgroundColor, setBackgroundColor] = useState('#9b9b9b');
-    const [newBoardGameType, setNewBoardGameType] = useState('');
-    const [newBoardGameName, setNewBoardGameName] = useState('');
-    const [addedBoardGameList, setAddedBoardGameList] = useState([]);
-    const boardGameTypeList = [
-        { type: '派對', color: '#efd100d6' },
-        { type: '策略', color: '#00efe3' },
-        { type: '情境', color: '#58ef00' },
-        { type: '戰爭', color: '#5400ef' },
-        { type: '抽象', color: '#e700ef' },
-        { type: '交換卡片', color: '#ef0034' },
-        { type: '兒童', color: '#0070ef' },
-        { type: '家庭', color: '#ef4400' },
-        { type: '其他', color: '#1000ef' },
-    ];
-
-    const setBoardGameColor = (boardGameType) => {
-        const { color } = boardGameTypeList.find(
-            ({ type }) => type === boardGameType
-        );
-        setBackgroundColor(color);
-    };
-
-    const handleChangeSelectBoardGameType = (e) => {
-        const { value } = e.target;
-        setBoardGameColor(value);
-        setNewBoardGameType(value);
-    };
-
-    const handleClickButtonNewBoardGame = () => {
-        if (!newBoardGameType) return alert('請選擇桌遊類型');
-        if (!newBoardGameName) return alert('請輸入桌遊名稱');
-        const newAddedBoardGameList = [
-            ...addedBoardGameList,
-            {
-                type: newBoardGameType,
-                name: newBoardGameName,
-                background: backgroundColor,
-            },
-        ];
-        setAddedBoardGameList(newAddedBoardGameList);
-        setNewBoardGameType('');
-        setNewBoardGameName('');
-        setBackgroundColor('#9b9b9b');
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        await postNewActiveBoardGame({
-            activeType: props.activeType,
-            date,
-            population,
-            boardGameType,
-            title,
-            content,
-        });
-    };
 
     return (
-        <Form onSubmit={handleSubmit}>
-            <Label>
-                <Icon src={IconCalendar} />
-                <Input type="datetime-local" required />
-            </Label>
-            <Label>
-                <Icon src={IconTitle} />
-                <Input type="text" required />
-            </Label>
-            <Label>
-                <Icon src={IconPopulation} />
-                <Input type="number" required />
-            </Label>
-            <Label>
-                <Icon src={IconPrice} />
-                <Input type="number" required />
-            </Label>
-            <TextArea
-                placeholder="補充說明..."
-                value={content}
-                onChange={(e) => setContent(e.target.value)}
-            ></TextArea>
-            <NewBoardGame>
-                <SelectBoardGameType
-                    style={{ background: backgroundColor }}
-                    value={newBoardGameType}
-                    onChange={handleChangeSelectBoardGameType}
-                >
-                    <option value="" hidden>
-                        請選擇桌遊類型
-                    </option>
-                    {boardGameTypeList.map(({ type }) => (
-                        <option key={type} value={type}>
-                            {type}
-                        </option>
-                    ))}
-                </SelectBoardGameType>
-                <InputBoardGameName
-                    placeholder="桌遊名稱..."
-                    type="text"
-                    value={newBoardGameName}
-                    onChange={(e) => setNewBoardGameName(e.target.value)}
-                />
-                <ButtonNewBoardGame
-                    style={{ backgroundColor: backgroundColor }}
-                    onClick={handleClickButtonNewBoardGame}
-                >
-                    新增
-                </ButtonNewBoardGame>
-            </NewBoardGame>
-            <AddedBoardGame>
-                {addedBoardGameList.map((boardGame, i) => {
-                    const { type, name, background } = boardGame;
-                    return (
-                        <AddedBoardGameItem
-                            key={i}
-                            style={{ background: background }}
-                        >
-                            <span style={{ marginRight: '10px' }}>
-                                {type}
-                                {name}
-                            </span>
-                            <BtnDeleteTag
-                                onClick={() => {
-                                    const newAddedBoardGameList = [
-                                        ...addedBoardGameList,
-                                    ];
-                                    newAddedBoardGameList.splice(i, 1);
-                                    setAddedBoardGameList(
-                                        newAddedBoardGameList
-                                    );
-                                }}
-                            >
-                                X
-                            </BtnDeleteTag>
-                        </AddedBoardGameItem>
-                    );
-                })}
-            </AddedBoardGame>
+        <Dummy>
             <Label>
                 <Icon src={IconPlace} />
                 <Input
@@ -316,9 +161,7 @@ const Boardgame = (props) => {
                     </Mark>
                 </GoogleMapReact>
             </GoogleMap>
-
-            <Button type="submit">新增</Button>
-        </Form>
+        </Dummy>
     );
 };
 const Img = styled.img``;
@@ -336,7 +179,7 @@ const Label = styled.label`
     display: flex;
     margin-bottom: 20px;
 `;
-const Form = styled.form`
+const Dummy = styled.form`
     max-width: 768px;
     display: flex;
     flex-direction: column;
@@ -370,81 +213,4 @@ const Input = styled(InputClearDefault)`
     font-size: 25px;
 `;
 
-const NewBoardGame = styled.div`
-    display: flex;
-`;
-const SelectBoardGameType = styled(SelectClearDefault)`
-    margin-bottom: 20px;
-    font-size: 18px;
-    color: #ffffff;
-`;
-const InputBoardGameName = styled(Input)`
-    font-size: 20px;
-`;
-const ButtonNewBoardGame = styled.div`
-    width: 90px;
-    padding: 10px 15px;
-    margin-bottom: 20px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 5px;
-    font-size: 18px;
-    color: #ffffff;
-    box-sizing: border-box;
-
-    &:hover {
-        opacity: 0.7;
-        cursor: pointer;
-    }
-    &:active {
-        opacity: 1;
-    }
-`;
-
-const AddedBoardGame = styled.div`
-    display: flex;
-    margin-bottom: 20px;
-    padding: 8px;
-`;
-
-const AddedBoardGameItem = styled.div`
-    margin-right: 10px;
-    padding: 8px 12px;
-    border-radius: 5px;
-    font-size: 18px;
-    color: white;
-`;
-
-const TextArea = styled(TextareaClearDefault)`
-    width: 100%;
-    height: 300px;
-    margin-bottom: 20px;
-    font-size: 20px;
-    border: 0px;
-`;
-const Button = styled(ButtonClearDefault)`
-    width: 80px;
-    margin-left: auto;
-    padding: 10px 15px;
-    border-radius: 5px;
-    color: white;
-    background: rgb(14, 145, 210);
-    font-size: 15px;
-
-    &:hover {
-        background: rgb(90, 176, 219);
-    }
-    &:active {
-        background: rgb(14, 145, 210);
-    }
-`;
-
-const BtnDeleteTag = styled.span`
-    &:hover {
-        color: grey;
-        cursor: pointer;
-    }
-`;
-
-export default Boardgame;
+export default GoogleMap;

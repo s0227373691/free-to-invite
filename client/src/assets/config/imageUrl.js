b18615206d5fbc919723c45f8bae170e4a62bb35
Elmer <s0227373691@gms.tku.edu.tw>
2020-12-19 16:55:26 +0800

    重購 統一由imageUrl.js 引用圖片

---
+++ client/src/assets/config/imageUrl.js
@@ -0,0 +1,17 @@
+import Price from '../svg/price';
+import Calendar from '../svg/calendar';
+import Title from '../svg/title';
+import Place from '../svg/place';
+import Population from '../svg/population';
+import Badminton from '../svg/shuttlecock';
+import Net from '../svg/net';
+import Strength from '../svg/strength';
+
+export const IconPrice = Price;
+export const IconCalendar = Calendar;
+export const IconTitle = Title;
+export const IconPlace = Place;
+export const IconPopulation = Population;
+export const IconBadminton = Badminton;
+export const IconNet = Net;
+export const IconStrength = Strength;

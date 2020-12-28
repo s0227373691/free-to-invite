677268d1e86fb8076413907b3c6fe51c533f1387
js0731 <a0905050792@gmail.com>
2020-12-28 16:15:44 +0800

    update file

--- client/src/components/newActive/movie.jsx
+++ client/src/components/newActive/movie.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
+import { SelectClearDefault } from '../styles/selects';
 import { ButtonClearDefault } from '../styles/buttons';
 import { TextareaClearDefault } from '../styles/textarea';
 import { InputClearDefault } from '../styles/inputs';
@@ -12,6 +13,24 @@ import IconPlace from '../../assets/svg/place';
 import IconPopulation from '../../assets/svg/population';
 
 const Movie = () => {
+    const [addedMovieList, setAddedMovieList] = useState([]);
+    const movieTypeList = [
+        { type: '動作' },
+        { type: '警匪' },
+        { type: '醫學' },
+        { type: '功夫' },
+        { type: '運動' },
+        { type: '戰爭' },
+        { type: '歷史' },
+        { type: '災難' },
+        { type: '喜劇' },
+        { type: '愛情' },
+        { type: '色情' },
+        { type: '驚悚' },
+        { type: '懸疑' },
+        { type: '恐怖' },
+        { type: '卡通' },
+    ];
     return (
         <Form>
             <Label>
@@ -35,6 +54,40 @@ const Movie = () => {
                 <Icon src={IconPrice} />
                 <Input id="price" placeholder="參加費用" type="number" />
             </Label>
+            <NewMovie>
+                <SelectMovieType>
+                    <option value="" hidden>
+                        請選擇桌遊類型
+                    </option>
+                </SelectMovieType>
+                <InputMovieName placeholder="電影名稱..." type="text" />
+                <ButtonNewMovie>新增</ButtonNewMovie>
+            </NewMovie>
+            <AddedMovie>
+                {addedMovieList.map((movie, i) => {
+                    return (
+                        <AddedMovieItem key={i}>
+                            <span style={{ marginRight: '10px' }}>
+                                {'類型'}
+                                {'name'}
+                            </span>
+                            <BtnDeleteTag
+                            // onClick={() => {
+                            //     const newAddedBoardGameList = [
+                            //         ...addedBoardGameList,
+                            //     ];
+                            //     newAddedBoardGameList.splice(i, 1);
+                            //     setAddedBoardGameList(
+                            //         newAddedBoardGameList
+                            //     );
+                            // }}
+                            >
+                                X
+                            </BtnDeleteTag>
+                        </AddedMovieItem>
+                    );
+                })}
+            </AddedMovie>
 
             <TextArea placeholder="補充說明..."></TextArea>
             <Button type="submit">新增</Button>
@@ -88,4 +141,51 @@ const Icon = styled.img`
     width: 40px;
 `;
 
+const NewMovie = styled.div`
+    display: flex;
+`;
+const SelectMovieType = styled(SelectClearDefault)`
+    margin-bottom: 20px;
+    font-size: 18px;
+    color: #ffffff;
+`;
+
+const InputMovieName = styled(Input)`
+    font-size: 20px;
+`;
+
+const AddedMovie = styled.div`
+    display: flex;
+    margin-bottom: 20px;
+    padding: 8px;
+`;
+
+const AddedMovieItem = styled.div`
+    margin-right: 10px;
+    padding: 8px 12px;
+    border-radius: 5px;
+    font-size: 18px;
+    color: white;
+`;
+
+const ButtonNewMovie = styled.div`
+    width: 90px;
+    padding: 10px 15px;
+    margin-bottom: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 5px;
+    font-size: 18px;
+    color: #ffffff;
+    box-sizing: border-box;
+
+    &:hover {
+        opacity: 0.7;
+        cursor: pointer;
+    }
+    &:active {
+        opacity: 1;
+    }
+`;
 export default Movie;

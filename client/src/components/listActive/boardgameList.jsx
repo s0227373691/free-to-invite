0d67adda522077a9977d80117977ebf12853c7f2
js0731 <a0905050792@gmail.com>
2021-02-18 14:57:50 +0800

    update file

--- client/src/components/listActive/boardgameList.jsx
+++ client/src/components/listActive/boardgameList.jsx
@@ -7,29 +7,34 @@ const boardgameList = () => {
     const [boardgameList, setBoardgameList] = useState([]);
 
     useEffect(() => {
-        const data = () => getBoardGameListData();
-        console.log(data);
-        setBoardgameList(data);
-
-        console.log(boardgameList);
+        const fetchData = async () => {
+            const data = await getBoardGameListData();
+            console.log(data.boardgameList);
+            await setBoardgameList(data.boardgameList);
+        };
+        fetchData();
     }, []);
     return (
         <BoardgameListRoot>
-            <Title>桌遊</Title>
+            <Title onClick={() => console.log(boardgameList)}></Title>
+
             <List>
-                {/* {boardgameList.map(({ title }) => {
-                    <div>{title}</div>;
-                })} */}
-                {/* {activeTypeList.map(({ type, activeList }) => (
-                    <optgroup key={type} label={type}>
-                        {activeList.map(({ name }) => (
-                            <option key={name} value={name}>
-                                {name}
-                            </option>
-                        ))}
-                    </optgroup>
-                ))} */}
-                <Item></Item>
+                {boardgameList
+                    ? boardgameList.map((item, index) => {
+                          const {
+                              _id,
+                              title,
+                              content,
+                              startDate,
+                              endDate,
+                              cost,
+                              place,
+                              population,
+                              //   addedBoardGameList,
+                          } = item;
+                          return <Item key={_id}>{title}</Item>;
+                      })
+                    : null}
             </List>
         </BoardgameListRoot>
     );
